f746bfb976f594edc3637b831bed0fcfd3566bf8
Ardy Dedase <ardy.dedase@viki.com>
2020-11-27 08:40:22 +0800

    functions as data

--- src/functionsVariable/index.ts
+++ src/functionsVariable/index.ts
@@ -1,5 +1,49 @@
-const sayHello = () => console.log('hello')
+// NOTE: Assign function to variable
 
-const sayHello2 = sayHello
+const getSum = (a: number, b: number): number => a + b
 
-sayHello2()
+console.log(getSum(1, 2))
+
+// Assign to another variable
+const addNumbers = getSum
+
+console.log(addNumbers(1, 2))
+
+// NOTE: Dynamically change the behavior of a function
+const isLocal: boolean = true
+const displayRunning = isLocal  
+    ? () => console.log('Running locally') 
+    : () => console.log('Not running locally')
+
+displayRunning()
+
+// NOTE: Example when used in mocking
+const ENV: string = 'development'
+
+// Live async call
+const fetchData = () => {
+    // time consuming live async call here.
+}
+
+// Mocked async call
+const fetchMockData = (): {name: string, title: string} => ({
+    name: 'Ardy',
+    title: 'Software Engineer', 
+})
+
+const fetchDataForTesting = ENV == 'development' 
+    ? fetchMockData
+    : fetchData
+
+console.log(fetchDataForTesting())
+
+// NOTE: Example A/B testing or experimentation
+const variantA = () => console.log('show variant A')
+const variantB = () => console.log('show variant B')
+
+const variantFunctionMap: object = {
+    'A': variantA, 
+    'B': variantB,
+}
+
+variantFunctionMap['A']()
