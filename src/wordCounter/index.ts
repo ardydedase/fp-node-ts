24fb4ab143473eb48a35c5a26d4a3d93d0273ad4
ardydedase <>
2021-02-20 20:47:46 -0600

    non functional solution

--- src/wordCounter/index.ts
+++ src/wordCounter/index.ts
@@ -6,15 +6,29 @@ That cannot fly
 Hold fast to dreams
 For when dreams go
 Life is a barren field
-Frozen with snow`.replace(/(\r\n|\n|\r)/gm, ' ')
+Frozen with snow`.replace(/(\r\n|\n|\r)/gm, ' ').toLowerCase()
 
-console.log(poemInput.split(' '))
-
-const countWords = words => {
-  return words.reduce((wordCounter, word) => ({
+// Functional approach
+const countWords = (words: Array<string>) => {
+  return words.reduce((wordCounter: Map<string, number>, word: string) => ({
     ...wordCounter,
     [word]: wordCounter[word] ? wordCounter[word] + 1: 1
-  }), {})
+  }), new Map<string, number>())
 }
 
 console.log(countWords(poemInput.split(' ')))
+
+// Non-functional approach
+let wordCounter = new Map<string, number>()
+const countWordsNonFunc = words => {
+  for (let word of words) {
+    if (wordCounter.get(word) > 0) {
+      wordCounter.set(word, wordCounter.get(word) + 1)
+    } else {
+      wordCounter.set(word, 1)
+    }
+  }
+  console.log(wordCounter)
+}
+
+countWordsNonFunc(poemInput.split(' '))
